5df2a765eda86c567fa77f38104792ac7a99504b
ApurbaDa <apurba.soft@gmail.com>
2025-08-11 15:47:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const scraper = require('./scraper')
+const scraper = require('./scraper');
 const app = express();
 
-//Home page
+// Home page
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-//API route
+// API route
 app.get('/api/search', (req, res) => {
     scraper.youtube(req.query.q, req.query.key, req.query.pageToken)
         .then(x => res.json(x))
         .catch(e => res.send(e));
 });
 
-app.listen(process.env.PORT || 8080, function () {
-  console.log(`Listening on port ${PORT}`);
+const PORT = process.env.PORT || 8080; // <-- store it
+
+app.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`);
 });
 
 module.exports = app;
